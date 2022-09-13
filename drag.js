4c5111ca84c4ca37e40831cf8063e979f6c669a5
Rod-aDev <100006082+Rod-aDev@users.noreply.github.com>
2022-09-12 18:38:24 -0600

    Update drag.js

--- drag.js
+++ drag.js
@@ -1,5 +1,35 @@
 const CARDS = 10;
 
 fetch('https://pokeapi.co/api/v2/ability/1/')
-.then(res => res.json())
+.then(res => res.json())const CARDS = 3;
+
+for(let i=1; i<= CARDS; i++){
+    let id = getRandomId(150)
+    searchPokemonById(id)
+}
+
+function getRandomId(max){
+    return Math.floor(Math.random()*max)+1
+}
+
+let draggableElements = document.querySelector('.draggable-elements')
+
+let pokemonSearched = [];
+async function searchPokemonById(id){
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    const data = await res.json()
+    pokemonSearched.push(data)
+
+    draggableElements.innerHTML = ''
+    pokemonSearched.forEach(pokemon =>{
+        console.log(pokemon.sprites.back_default)
+       draggableElements.innerHTML +=
+        `<div class="pokemon">
+        <img class="image" 
+        src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
+        </div>`
+    } )
+}
+
+
 .then(data => console.log(data))
